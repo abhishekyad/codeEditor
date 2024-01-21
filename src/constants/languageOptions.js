d0a804b061b274392dbb504d7aff52fc23581604
Abhishek Yadav <31823769+abhishekyad@users.noreply.github.com>
2024-01-22 03:37:56 +0530

    Add files via upload

---
+++ src/constants/languageOptions.js
@@ -0,0 +1,20 @@
+export const languageOptions = [
+  {
+    id: 63,
+    name: "JavaScript (Node.js 12.14.0)",
+    label: "JavaScript (Node.js 12.14.0)",
+    value: "javascript",
+  },
+  {
+    id: 45,
+    name: "Python 3",
+    label: "Python 3",
+    value: "python",
+  },
+  {
+    id: 85,
+    name: "JAVA",
+    label: "JAVA",
+    value: "java",
+  }
+];
